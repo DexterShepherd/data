164f35109ae06be19ff036d9b2b91004dcd739ca
Dexter Shepherd <dexterjshepherd@gmail.com>
2018-02-22 00:54:57 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+import Reveal from 'reveal.js'
+import p5 from 'p5'
+import './node_modules/reveal.js/css/reveal.css'
+import'./node_modules/reveal.js/css/theme/white.css'
+
+Reveal.initialize({
+  slideNumber: true,
+  history: true
+})
